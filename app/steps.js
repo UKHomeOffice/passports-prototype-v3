e2ec2ab1cc89a334bb451fb00df26515ea501f31
Vee <v.razazi@kainos.com>
2020-08-10 14:50:15 +0100

    heroku fix attempt

--- app/steps.js
+++ app/steps.js
@@ -1114,7 +1114,7 @@ const dps = {
         resetJourney: true,
         next: '/filter/overseas'
     },
-    '/dps/dps-not-eligible':{
+    '/DPS/dps-not-eligible':{
     },
     '/dps/dps-lost-or-stolen-choose-different-service':{
         next:'/filter/begin'
